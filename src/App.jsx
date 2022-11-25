e58b6deaa57b0931d880e2cc81c26dd5a6ab14a7
YM CHOI <escriboy@gmail.com>
2022-11-25 23:37:20 +0900

    1125

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,69 @@
-import React from "react";
-import InputSample from "./InputSample";
+import React, { useRef, useState } from "react";
+import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-  return <InputSample />;
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@exampl.com",
+    },
+  ]);
+
+  const nextId = useRef(4);
+  //useRef() 를 사용 할 때 파라미터를 넣어주면, 이 값이 .current 값의 기본값이 된다.
+  const onCreate = () => {
+    // 나중에 구현할 배열에 항목을 추가하는 로직
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // setUsers([...users, user]);
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
