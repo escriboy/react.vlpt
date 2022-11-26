9dbc9ffaa643209662ab2d26e324605add07924f
YM CHOI <escriboy@gmail.com>
2022-11-27 00:34:59 +0900

    1125

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,19 @@ function App() {
       id: 1,
       username: "velopert",
       email: "public.velopert@gmail.com",
+      active: true,
     },
     {
       id: 2,
       username: "tester",
       email: "tester@example.com",
+      active: false,
     },
     {
       id: 3,
       username: "liz",
       email: "liz@exampl.com",
+      active: false,
     },
   ]);
 
@@ -59,6 +62,14 @@ function App() {
     setUsers(users.filter((user) => user.id !== id));
   };
 
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
   return (
     <>
       <CreateUser
@@ -67,7 +78,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function User({ user, onRemove }) {
+function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log("user 값이 설정됨");
+    console.log(user);
+    return () => {
+      console.log("user가 바뀌기 전...");
+      console.log(user);
+    };
+  }, [user]);
+  // deps 파라미터를 생략하면 컴포넌트가 리렌더링 될 때마다 호출된다.
+  // useEffect(() => {
+  //   console.log(user);
+  // });
+
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} onRemove={onRemove} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
