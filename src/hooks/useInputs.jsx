92c112fa1750ceff65c7d00195a6eb1d90816c4e
YM CHOI <escriboy@gmail.com>
2022-11-28 21:09:54 +0900

    useContext

--- src/hooks/useInputs.jsx
+++ src/hooks/useInputs.jsx
@@ -1,14 +1,12 @@
-import { useCallback, useState } from "react";
+import React, { useState, useCallback } from "react";
 
 function UseInputs(initialForm) {
   const [form, setForm] = useState(initialForm);
-
   // change
   const onChange = useCallback((e) => {
     const { name, value } = e.target;
     setForm((form) => ({ ...form, [name]: value }));
   }, []);
-
   const reset = useCallback(() => setForm(initialForm), [initialForm]);
 
   return [form, onChange, reset];
